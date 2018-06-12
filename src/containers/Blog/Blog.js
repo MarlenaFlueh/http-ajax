da46fcc6a1cbea4ab05d5a574e46a533aed2ab1f
MarlenaFlueh <mf@esyoil.com>
2018-06-12 19:30:52 +0200

    implement post select option

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import "./Blog.css";
 
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [],
+    selectedPostId: null
   };
 
   async componentDidMount() {
@@ -22,16 +23,25 @@ class Blog extends Component {
     this.setState({ posts });
   }
 
+  postSelectedHandler = selectedPostId => {
+    this.setState({ selectedPostId });
+  };
+
   render() {
     const posts = this.state.posts.map(post => (
-      <Post key={post.id} title={post.title} author={post.author} />
+      <Post
+        key={post.id}
+        title={post.title}
+        author={post.author}
+        clicked={() => this.postSelectedHandler(post.id)}
+      />
     ));
 
     return (
       <div>
         <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
