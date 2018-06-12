0990c072fec13cbd5423fe43ab7ee16979dbc06f
MarlenaFlueh <mf@esyoil.com>
2018-06-12 21:55:43 +0200

    add delete axios method

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -22,6 +22,13 @@ class FullPost extends Component {
     }
   }
 
+  deletePostHandler = async () => {
+    const post = await axios.delete(
+      `https://jsonplaceholder.typicode.com/posts/${this.props.id}`
+    );
+    console.log(post);
+  };
+
   render() {
     let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
     if (this.props.id) {
@@ -33,7 +40,9 @@ class FullPost extends Component {
           <h1>{this.state.loadedPost.title}</h1>
           <p>{this.state.loadedPost.body}</p>
           <div className="Edit">
-            <button className="Delete">Delete</button>
+            <button className="Delete" onClick={this.deletePostHandler}>
+              Delete
+            </button>
           </div>
         </div>
       );
