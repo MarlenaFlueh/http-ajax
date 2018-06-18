cf8ec1c443d142f20c6017be4ddd43555cf6ef38
MarlenaFlueh <mf@esyoil.com>
2018-06-18 21:38:21 +0200

    change structure for route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,49 +1,10 @@
 import React, { Component } from "react";
 
-import axios from "../../axios";
-import Post from "../../components/Post/Post";
-import FullPost from "../../components/FullPost/FullPost";
-import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
+import Posts from "./Posts/Posts";
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null,
-    error: false
-  };
-
-  async componentDidMount() {
-    try {
-      const { data } = await axios.get("/posts");
-
-      const shortenedPosts = data.slice(0, 4);
-      const posts = shortenedPosts.map(post => {
-        return { ...post, author: "Marlena" };
-      });
-      this.setState({ posts });
-    } catch (error) {
-      this.setState({ error: true });
-    }
-  }
-
-  postSelectedHandler = selectedPostId => {
-    this.setState({ selectedPostId });
-  };
-
   render() {
-    let posts = <p styled={{ textAlign: "center" }}>Something went wrong!</p>;
-    if (!this.state.error) {
-      posts = this.state.posts.map(post => (
-        <Post
-          key={post.id}
-          title={post.title}
-          author={post.author}
-          clicked={() => this.postSelectedHandler(post.id)}
-        />
-      ));
-    }
-
     return (
       <div className="Blog">
         <header>
@@ -58,7 +19,7 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <section className="Posts">{posts}</section>
+        <Posts />
       </div>
     );
   }
---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+import axios from "../../../axios";
+import Post from "../../../components/Post/Post";
+import "./Posts.css";
+
+class Posts extends Component {
+  state = {
+    posts: []
+  };
+
+  async componentDidMount() {
+    try {
+      const { data } = await axios.get("/posts");
+
+      const shortenedPosts = data.slice(0, 4);
+      const posts = shortenedPosts.map(post => {
+        return { ...post, author: "Marlena" };
+      });
+      this.setState({ posts });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  postSelectedHandler = selectedPostId => {
+    this.setState({ selectedPostId });
+  };
+
+  render() {
+    let posts = <p styled={{ textAlign: "center" }}>Something went wrong!</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}
+        />
+      ));
+    }
+    return <section className="Posts">{posts}</section>;
+  }
+}
+
+export default Posts;
